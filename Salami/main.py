0380f52e1e9147d06f041012774374c1d7dfd020
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-17 22:37:27 +0800

    Added main.py

---
+++ Salami/main.py
@@ -0,0 +1,20 @@
+
+import arcade
+
+WIDTH = 600
+HEIGHT = 400
+TITLE = "Python Game Jam 2020"
+
+class PyGameJam2020(arcade.Window):
+
+    def _init__(self):
+        super().__init__(WIDTH, HEIGHT, TITLE)
+
+    def setup(self):
+        pass
+
+    def on_update(self, delta):
+        pass
+
+    def on_draw(self):
+        pass
