c95bdbe5abd9d32ae28f53e9ca73383b47ed932d
sachapomme <sacha02.rolle@gmail.com>
2020-04-17 20:41:51 +0200

    adds __main__.py

---
+++ beanoculars/game/__main__.py
@@ -0,0 +1,15 @@
+import arcade
+
+import game.setup
+from game.constants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE
+
+def main() -> None:
+    """ Main method """
+    window = game.setup.MyGame(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_TITLE)
+    print('Window created')
+    window.setup()
+    print('Game set up')
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
