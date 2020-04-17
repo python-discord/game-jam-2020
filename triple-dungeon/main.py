99c0d9817de61a22d94dce66f7d52c558e85c3eb
Cameron Smart <smartcameron@gmail.com>
2020-04-17 14:46:15 -0700

    Added movement controls w/ nonstick keys

--- triple-dungeon/main.py
+++ triple-dungeon/main.py
@@ -6,7 +6,14 @@ import arcade
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
+SCREEN_TITLE = "Triple Dungeon!!!"
+
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 5
 
 
 class MyGame(arcade.Window):
@@ -19,17 +26,92 @@ class MyGame(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        # These are 'lists' that keep track of our sprites. Each sprite should
+        # go into a list.
+        self.wall_list = None
+        self.player_list = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+        #list to keep track of keypresses
+        self.prev_keypress = []
+
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
-        pass
+        # Create the Sprite lists
+        self.wall_list = arcade.SpriteList()
+        self.player_list = arcade.SpriteList()
+
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "../Python Arcade logo.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
 
     def on_draw(self):
         """ Render the screen. """
 
+        # Clear the screen to the background color
         arcade.start_render()
-        # Code to draw the screen goes here
+
+        # Draw our sprites
+        self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+            self.prev_keypress.append(key)
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+            self.prev_keypress.append(key)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.prev_keypress.append(key)
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            self.prev_keypress.append(key)
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+            self.prev_keypress.remove(key)
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+            self.prev_keypress.remove(key)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+            self.prev_keypress.remove(key)
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+            self.prev_keypress.remove(key)
+
+        if self.prev_keypress:
+            self.on_key_press(self.prev_keypress.pop(0), '')
+
+
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+
 
 
 def main():
