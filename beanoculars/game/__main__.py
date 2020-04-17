4d64bbd650bb29f4f198313cec097c83dd143ee4
sachapomme <sacha02.rolle@gmail.com>
2020-04-17 21:20:15 +0200

    updated imports

--- beanoculars/game/__main__.py
+++ beanoculars/game/__main__.py
@@ -1,11 +1,12 @@
+# main game file
 import arcade
 
-import game.setup
-from game.constants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE
+import game
+from constants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE
 
 def main() -> None:
     """ Main method """
-    window = game.setup.MyGame(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_TITLE)
+    window = game.MyGame(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_TITLE)
     print('Window created')
     window.setup()
     print('Game set up')
