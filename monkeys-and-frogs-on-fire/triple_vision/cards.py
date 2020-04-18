209abc145e2ca4e983f948982c44a2c7754c96e8
Den4200 <dpham.42@hotmail.com>
2020-04-18 01:33:46 -0400

    create card ui

---
+++ monkeys-and-frogs-on-fire/triple_vision/cards.py
@@ -0,0 +1,83 @@
+import arcade
+
+from triple_vision.constants import SCALING, WINDOW_SIZE
+
+
+
+class CardManager:
+
+	def __init__(self, ctx) -> None:
+		self.ctx = ctx
+
+		self.cards = arcade.SpriteList()
+		self.colors = ('red', 'green', 'blue')
+
+		card_scale = SCALING / 6
+
+		self.MIN_CARD_HEIGHT = -132 * card_scale
+		self.MAX_CARD_HEIGHT = 280 * card_scale
+
+		for idx, color in enumerate(self.colors):
+			self.cards.append(
+				arcade.Sprite(
+					filename=f'assets/wizard/{color}_card.png',
+					scale=card_scale,
+					center_x=WINDOW_SIZE[0] / 2 + (idx - 1) * 400 * card_scale,
+					center_y=self.MIN_CARD_HEIGHT
+				)
+			)
+
+		self.show_cards = False
+
+	def check_mouse_motion(self, x, y) -> None:
+		if (
+			self.cards[0].left < x < self.cards[-1].right and
+			self.cards[0].bottom < y < self.cards[-1].top
+		):
+			self.show_cards = True
+			self.ctx.paused = True
+
+		else:
+			self.show_cards = False
+			self.ctx.paused = False
+
+	def check_mouse_press(self, x, y, button) -> bool:
+		if button == arcade.MOUSE_BUTTON_LEFT:
+			if (
+				self.cards[0].left < x < self.cards[-1].right and
+				self.cards[0].bottom < y < self.cards[-1].top
+			):
+
+				for idx, card in enumerate(self.cards):
+					if (
+						card.left < x < card.right and
+						card.bottom < y < card.top
+					):
+						self.ctx.player.cur_color = self.colors[idx]
+						self.show_cards = False
+						self.ctx.paused = False
+
+				return True
+
+		return False
+
+	def draw(self) -> None:
+		self.cards.draw()
+
+	def update(self, delta_time: float = 1/60):
+		if not self.show_cards:
+			for card in self.cards:
+				card.change_y = -10
+
+		elif self.show_cards:
+			for card in self.cards:
+				card.change_y = 10
+
+		if (
+			(self.show_cards and self.cards[0].center_y >= self.MAX_CARD_HEIGHT) or
+			(not self.show_cards and self.cards[0].center_y <= self.MIN_CARD_HEIGHT)
+		):
+			for card in self.cards:
+				card.change_y = 0
+
+		self.cards.update()
--- monkeys-and-frogs-on-fire/triple_vision/triple_vision.py
+++ monkeys-and-frogs-on-fire/triple_vision/triple_vision.py
@@ -2,6 +2,7 @@ import random
 
 import arcade
 
+from triple_vision.cards import CardManager
 from triple_vision.constants import (
     SCALED_TILE,
     SCALING,
@@ -19,9 +20,14 @@ class TripleVision(arcade.View):
     def __init__(self) -> None:
         super().__init__()
 
+        self.paused = False
+
         self.tiles = None
+
         self.player = None
         self.enemy = None
+
+        self.card_manager = None
 
         self.physics_engine = None
 
@@ -41,16 +47,25 @@ class TripleVision(arcade.View):
 
         self.player = Player('m')
         self.enemy = ChasingEnemy(Enemies.big_demon, self.player, 1, SCALED_TILE * 10, center_x=50, center_y=500)
+        self.card_manager = CardManager(self)
+
+    def on_mouse_motion(self, x, y, dx, dy) -> None:
+        self.card_manager.check_mouse_motion(x, y)
 
     def on_mouse_press(self, x, y, button, modifiers) -> None:
-        if button == arcade.MOUSE_BUTTON_LEFT:
-            self.player.move_to(x, y, rotate=False)
+        if not self.card_manager.check_mouse_press(x, y, button):
+            if button == arcade.MOUSE_BUTTON_LEFT:
+                self.player.move_to(x, y, rotate=False)
 
     def on_draw(self) -> None:
         self.tiles.draw()
         self.player.draw()
         self.enemy.draw()
+        self.card_manager.draw()
 
     def on_update(self, delta_time: float) -> None:
-        self.player.update(delta_time)
-        self.enemy.update()
+        if not self.paused:
+            self.player.update(delta_time)
+            self.enemy.update(delta_time)
+
+        self.card_manager.update()
