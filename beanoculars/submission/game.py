54428e91482eeada8edbe3176d828f79bcaebdcd
sachapomme <sacha02.rolle@gmail.com>
2020-04-18 15:30:53 +0200

    new map and "fullscreen" (more of a focus mode) features

--- beanoculars/submission/game.py
+++ beanoculars/submission/game.py
@@ -5,11 +5,13 @@ from submission.gameConstants import *
 from submission.loadAnimatedChars import *
 from submission.tileMapLoader import *
 from submission.sounds import loadSounds
+from random import randint
 import math
 
 
 def getGridCase(position):
-    case = [math.floor(position[0]/32),math.floor(position[1]/32)]
+    case = [math.floor(position[0]/32)-22*FULLSCREEN,math.floor(position[1]/32)-9*FULLSCREEN]
+    print(case)
     return case
 
 # Class defining
@@ -19,7 +21,10 @@ class MyGame(arcade.Window):
     """
 
     def __init__(self, width, height, windowTitle):
-        super(MyGame, self).__init__(width, height, windowTitle, resizable=False)
+        if FULLSCREEN:
+            super(MyGame, self).__init__(width, height, windowTitle, fullscreen=True)
+        else:
+            super(MyGame, self).__init__(width, height, windowTitle, fullscreen=False)
 
         self.ground_list = None
         self.path_list = None
@@ -33,30 +38,46 @@ class MyGame(arcade.Window):
         self.mouse_y = 0
 
         self.mouse_click = [0,0]
-        self.destination = [-1,-1]
+        self.destination = [0,0]
 
         self.sound_dict = {}
 
-        arcade.set_background_color(arcade.csscolor.PURPLE)
+        self.window_offset_x = 0
+        self.window_offset_y = 0
+
+        self.screen_x = 0
+        self.screen_y = 0
+
+        arcade.set_background_color((94, 132, 63))
 
     def setup(self):
         """ Set up the test here. Call this function to restart the test. """
+        if FULLSCREEN:
+            self.screen_x, self.screen_y = self.get_size()
+            self.window_offset_x = self.screen_x/2 - WINDOW_WIDTH/2
+            self.window_offset_y = self.screen_y/2 - WINDOW_HEIGHT/2
+
         self.player_list = arcade.SpriteList()
         self.ground_list = arcade.SpriteList()
         self.path_list = arcade.SpriteList()
 
         self.player_sprite = arcade.Sprite(PATH_ADD + "images\\sprite\\player.png", PLAYER_SCALING)
-        self.player_sprite.center_x = WINDOW_WIDTH/2
-        self.player_sprite.center_y = WINDOW_HEIGHT/2
+        self.player_sprite.center_x = self.destination[0] * TILE_SIZE + TILE_SIZE / 2 + self.window_offset_x
+        self.player_sprite.center_y = self.destination[1] * TILE_SIZE + TILE_SIZE / 2 + self.window_offset_y
         self.player_list.append(self.player_sprite)
 
         self.path_list = loadPathTilemap()
 
-        for x in range(0, WINDOW_WIDTH, TILE_SIZE):  # Crée le fond à l'aide de grassTile.png
+        for i in range(len(self.path_list)):
+            self.path_list[i].center_x += self.window_offset_x
+            self.path_list[i].center_y += self.window_offset_y
+
+        for x in range(0, WINDOW_WIDTH, TILE_SIZE):  # Crée le fond à l'aide des grasstiles
             for y in range(0, WINDOW_HEIGHT, TILE_SIZE):
-                ground = arcade.Sprite(PATH_ADD+"images\\tiles\\grassTile.png", TILE_SCALING)
-                ground.center_x = x + TILE_SIZE * TILE_SCALING / 2
-                ground.center_y = y + TILE_SIZE * TILE_SCALING / 2
+                randomNum = randint(1,3)
+                ground = arcade.Sprite(PATH_ADD+f"images\\tiles\\grassTile{randomNum}.png", TILE_SCALING)
+                ground.center_x = x + TILE_SIZE * TILE_SCALING / 2 + self.window_offset_x
+                ground.center_y = y + TILE_SIZE * TILE_SCALING / 2 + self.window_offset_y
                 self.ground_list.append(ground)
 
         loadSounds(PATH_ADD + "sounds", self.sound_dict)
@@ -77,8 +98,8 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time: float):
         """ On Update method"""
         if self.destination != [-1,-1]:
-            self.player_sprite.center_x = self.destination[0] * TILE_SIZE + TILE_SIZE/2
-            self.player_sprite.center_y = self.destination[1] * TILE_SIZE + TILE_SIZE/2
+            self.player_sprite.center_x = self.destination[0] * TILE_SIZE + TILE_SIZE/2 + self.window_offset_x
+            self.player_sprite.center_y = self.destination[1] * TILE_SIZE + TILE_SIZE/2 + self.window_offset_y
             self.destination = [-1,-1]
 
     def on_key_press(self, symbol: int, modifiers: int):
@@ -91,14 +112,28 @@ class MyGame(arcade.Window):
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         """ Get mouse's presses. """
-        self.mouse_click = [self.mouse_x, self.mouse_y]
-        self.destination = getGridCase(self.mouse_click)
-        if button == 1: # Si clique gauche
-            pass
-        elif button == 4: # Si clique droit
-            pass
+        if FULLSCREEN:
+            if self.mouse_x > self.screen_x/2 + WINDOW_WIDTH/2 or self.mouse_x < self.screen_x/2 - WINDOW_WIDTH/2:
+                print('Cannot move')
+            elif self.mouse_y > self.screen_y/2 + WINDOW_HEIGHT/2 or self.mouse_y < self.screen_y/2 - WINDOW_HEIGHT/2:
+                print('Cannot move')
 
-        self.sound_dict['testsound.wav'].play()
+            else:
+                self.mouse_click = [self.mouse_x, self.mouse_y]
+                self.destination = getGridCase(self.mouse_click)
+                if button == 1:  # Si clique gauche
+                    pass
+                elif button == 4:  # Si clique droit
+                    pass
+        else:
+            self.mouse_click = [self.mouse_x, self.mouse_y]
+            self.destination = getGridCase(self.mouse_click)
+            if button == 1: # Si clique gauche
+                pass
+            elif button == 4: # Si clique droit
+                pass
+
+
     def on_mouse_release(self, x: float, y: float, button: int,
                          modifiers: int):
         """ Get mouse's releases. """
