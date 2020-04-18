a22b472392104d55be070950d8e6ecce123ff4ea
Xevion <xevioni@yandex.com>
2020-04-18 01:25:36 -0500

    create new config class for organizing constants

--- triple-dungeon/main.py
+++ triple-dungeon/main.py
@@ -1,28 +1,4 @@
-"""
-Platformer Game
-"""
 import arcade
-
-# Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Triple Dungeon!!!"
-
-# Constants used to scale our sprites from their original size
-CHARACTER_SCALING = 1
-TILE_SCALING = 2
-
-# Movement speed of player, in pixels per frame
-PLAYER_MOVEMENT_SPEED = 5
-
-
-# How many pixels to keep as a minimum margin between the character
-# and the edge of the screen.
-LEFT_VIEWPORT_MARGIN = 250
-RIGHT_VIEWPORT_MARGIN = 250
-BOTTOM_VIEWPORT_MARGIN = 50
-TOP_VIEWPORT_MARGIN = 100
-
 
 class MyGame(arcade.Window):
     """
