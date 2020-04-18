8368d2de466eb16b78645fec4a4e077898c1d3a8
Kevin Sheng <kevinsheng2005@gmail.com>
2020-04-18 13:33:22 -0700

    started

--- gamer_gang/mainStuff.py
+++ gamer_gang/mainStuff.py
@@ -1,8 +1,24 @@
 import arcade
 
-class Game(arcade.Window):
-    pass
+width = 700
+height = 600
 
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(width, height, title='GAMER GANG GAMER GANG GAMER GANG')
+        arcade.set_background_color(arcade.color.BLUE_SAPPHIRE)
+
+        self.setup()
+
+    def setup(self):
+        pass
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.UP:
+            pass  # jump
+
+        elif symbol == arcade.key.LEFT:
+            pass  # move left
 
 def main():
     actualGame = Game()
