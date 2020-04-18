7154615687150ebd277a615e55de4bd8e5959ca6
sachapomme <sacha02.rolle@gmail.com>
2020-04-18 23:31:01 +0200

    top comment

--- beanoculars/submission/__main__.py
+++ beanoculars/submission/__main__.py
@@ -1,7 +1,12 @@
-# Main fuck file
+"""
+
+'Tricerasect' a game of the python-discord game jam 2020
+Made in 10 days, using the arcade library and based on
+the theme : Three of a kind
+Author: sachapomme
+
+"""
 import arcade
-import os
-import pathlib
 
 from submission.game import MyGame
 from submission.gameConstants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE
