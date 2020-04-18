4486e294047902833870d1fb8b220f724df6e0ed
sachapomme <sacha02.rolle@gmail.com>
2020-04-18 15:29:27 +0200

    new tileset

--- beanoculars/submission/tmx_sets/ts_ground.tsx
+++ beanoculars/submission/tmx_sets/ts_ground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="ts_ground" tilewidth="32" tileheight="32" tilecount="6" columns="3">
- <image source="../images/tiles/tilesetpic.png" width="96" height="64"/>
+<tileset version="1.2" tiledversion="1.3.4" name="tileset" tilewidth="32" tileheight="32" tilecount="12" columns="6">
+ <image source="../images/tiles/tilesetpic.png" width="192" height="64"/>
 </tileset>
