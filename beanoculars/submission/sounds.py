9d26c0b9e0687c8b0cd7de3fcc65f7ed857add42
sachapomme <sacha02.rolle@gmail.com>
2020-04-19 10:11:28 +0200

    updated path

--- beanoculars/submission/sounds.py
+++ beanoculars/submission/sounds.py
@@ -2,7 +2,7 @@ import arcade
 import os
 from submission.gameConstants import PATH
 
-def loadSounds(foldername: str, s_dict):
+def loadSounds(s_dict):
     os.chdir(PATH['sound'])
     for sound in os.listdir():
         s_dict[sound] = arcade.Sound(sound)
