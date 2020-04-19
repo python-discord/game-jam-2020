4386d61b7e5f59e108cf44d2655b52777242221e
mungarifr <mungarifr@gmail.com>
2020-04-19 15:57:05 +0200

    Scrolling

--- Binary/game.py
+++ Binary/game.py
@@ -14,40 +14,27 @@ import map_generator
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Starting Template"
-SPRITE_SCALING = 0.5
-SPRITE_NATIVE_SIZE = 128
-SPRITE_SIZE = int(SPRITE_NATIVE_SIZE * SPRITE_SCALING)
 
 def handle_left(self, press):
     if(press):
-        self.player.change_x = -2
+        self.player.change_x = -5
     else:
         self.player.change_x = 0
 def handle_right(self, press):
     if(press):
-        self.player.change_x = 2
+        self.player.change_x = 5
     else:
         self.player.change_x = 0
 def handle_up(self, press):
     if(press):
-        self.player.change_y = 2
+        self.player.change_y = 5
     else:
         self.player.change_y = 0
 def handle_down(self, press):
     if(press):
-        self.player.change_y = -2
+        self.player.change_y = -5
     else:
         self.player.change_y = 0
-def handle_space(self, press):
-    self.root = map_generator.Room(map_generator.ROOM_W, 
-                                map_generator.ROOM_H, 
-                                (random.randrange(0,255), 
-                                random.randrange(0,255), 
-                                random.randrange(0,255)), 
-                                [None]*map_generator.MAX_ROOM_NEIGH, 
-                                (0,0))
-                    
-    map_generator.generate([self.root], 0, 0)
 
 class MyGame(arcade.Window):
 
@@ -56,7 +43,6 @@ class MyGame(arcade.Window):
         arcade.key.D: handle_right,
         arcade.key.W: handle_up,
         arcade.key.S: handle_down,
-        arcade.key.SPACE: handle_space
     }
     """
     Main application class.
@@ -75,7 +61,6 @@ class MyGame(arcade.Window):
         self.RoomList = None
         self.player_list = None
         self.physics_engine = None
-        self.room = None
         self.current_room = None
         self.wall = None
         self.wall_list = None
@@ -84,33 +69,43 @@ class MyGame(arcade.Window):
 
     def setup(self):
         # Create your sprites and sprite lists here
+        self.root = map_generator.Room(map_generator.ROOM_W, 
+                                map_generator.ROOM_H, 
+                                (random.randrange(0,255), 
+                                random.randrange(0,255), 
+                                random.randrange(0,255)), 
+                                [None]*map_generator.MAX_ROOM_NEIGH, 
+                                (0,0),
+                                None)
+                    
+        map_generator.generate([self.root], 0, 0)
+
         self.player = arcade.SpriteSolidColor(40, 40, arcade.csscolor.BLACK)
         self.player.center_x = SCREEN_WIDTH // 2
         self.player.center_y = SCREEN_HEIGHT // 2
         self.player_list = arcade.SpriteList()
         self.player_list.append(self.player)
         self.wall_list = arcade.SpriteList()
+        self.current_room = self.root
 
-        for y in (0, SCREEN_HEIGHT - SPRITE_SIZE):
+        for y in (0, SCREEN_HEIGHT):
         # Loop for each box going across
-            for x in range(0, SCREEN_WIDTH, SPRITE_SIZE):
-                self.wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
+            for x in range(0, SCREEN_WIDTH):
+                self.wall = arcade.SpriteSolidColor(1,0,arcade.csscolor.WHITE)
                 self.wall.left = x
                 self.wall.bottom = y
                 self.wall_list.append(self.wall)
 
     # Create left and right column of boxes
-        for x in (0, SCREEN_WIDTH - SPRITE_SIZE):
+        for x in (0, SCREEN_WIDTH):
         # Loop for each box going across
-            for y in range(SPRITE_SIZE, SCREEN_HEIGHT - SPRITE_SIZE, SPRITE_SIZE):
-                # Skip making a block 4 and 5 blocks up
-                if (y != SPRITE_SIZE * 4 and y != SPRITE_SIZE * 5) or x != 0:
-                    self.wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", SPRITE_SCALING)
-                    self.wall.left = x
-                    self.wall.bottom = y
-                    self.wall_list.append(self.wall)
+            for y in range(0, SCREEN_WIDTH):
+                self.wall = arcade.SpriteSolidColor(1,0,arcade.csscolor.WHITE)
+                self.wall.left = x
+                self.wall.bottom = y
+                self.wall_list.append(self.wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.wall_list)    
 
     def on_draw(self):
         arcade.start_render()
@@ -123,8 +118,9 @@ class MyGame(arcade.Window):
 
         # Call draw() on all your sprite lists below
 
-        self.draw_rooms([self.root], 0, (-1,0))
+        self.draw_room_with_neighbors(self.current_room)
         self.player_list.draw()
+        #self.draw_room_smol([self.root], 1)
         self.wall_list.draw()
 
     def on_update(self, delta_time):
@@ -134,6 +130,8 @@ class MyGame(arcade.Window):
         need it.
         """
         self.physics_engine.update()
+        self.update_player_room()
+        self.set_viewport(self.player.center_x - SCREEN_WIDTH / 2, self.player.center_x + SCREEN_WIDTH / 2, self.player.center_y - SCREEN_HEIGHT / 2, self.player.center_y + SCREEN_HEIGHT / 2)
 
     def on_key_press(self, key, key_modifiers):
         if key in self.key_mapping:
@@ -151,18 +149,58 @@ class MyGame(arcade.Window):
         else:
             pass
 
-    def draw_rooms(self, rooms, count, idx):
+    def draw_room_with_neighbors(self, room):
+        self.draw_room(room)
+        for n in self.root.neighbors:
+            if n is not None:
+                self.draw_room(n)
+ 
+    def draw_room(self, room):
+        x, y = room.id
+        width, height = room.width, room.height
+        arcade.draw_rectangle_filled(x * width + width / 2, y * height + height / 2, width, height, room.color)
+ 
+    def update_player_room(self):
+        player_x = self.player.center_x
+        player_y = self.player.center_y
+ 
+        node_ix, node_iy = self.current_room.id
+        node_width = self.current_room.width
+        node_height = self.current_room.height
+ 
+        node_x = node_ix * node_width
+        node_y = node_iy * node_height
+ 
+        if player_x > node_x + node_width:
+            # go right
+            next_id = node_ix + 1, node_iy
+        elif player_x < node_x:
+            # go left
+            next_id = node_ix - 1, node_iy
+        elif player_y > node_y + node_height:
+            # go up
+            next_id = node_ix, node_iy + 1
+        elif player_y < node_y:
+            # go down
+            next_id = node_ix, node_iy - 1
+        else:
+            return
+ 
+        for n in self.current_room.neighbors:
+            if n is not None and n.id == next_id:
+                self.current_room = n
+                break
+
+"""    def draw_room_smol(self, rooms, count):
         if(count == map_generator.MAX_ROOM_NUM): 
             return
         else:
             for room in rooms:
                 for neighbor in room.neighbors:
-                    if (neighbor is not None and room.id == idx):
-                        arcade.draw_rectangle_filled(neighbor.x, neighbor.y, roneighborom.width, neighbor.height, neighbor.color)
-                        return
-                    else:
+                    if neighbor is not None:
+                        arcade.draw_rectangle_filled(SCREEN_WIDTH // 2 + (20 * count) , SCREEN_HEIGHT // 2 + (20 * count) // 2, 20, 20,neighbor.color)
                         count+=1
-                        self.draw_rooms([neighbor], count, neighbor.id)
+                        self.draw_rooms([neighbor], count)"""
 
 
 def main():
--- Binary/map_generator.py
+++ Binary/map_generator.py
@@ -10,22 +10,28 @@ ROOM_H = SCREEN_HEIGHT
 
 
 class Room():
-    def __init__(self, width, height, color, neighbors, id):
+    def __init__(self, width, height, color, neighbors, id, parent):
         self.width = width
         self.height = height
         self.color = color
         self.neighbors = neighbors
         self.id = id
-
+        self.parent = parent
 
 def generate(nodes, depth, count):
+    # If I reach max depth OR max count then I return
     if(depth > 5 or count > MAX_ROOM_NUM):
         return True
     else:
+        #Empty tmp list to hold new nodes
         new_node_list = []
+        #Basically iterating through each node I'm feeding to the func, the parents
         for node in nodes:
+            #Iteratign through each of its neighbours
             for index,branch in enumerate(node.neighbors):
-                if(branch == None and random.randint(1, 3) != 1 and count < MAX_ROOM_NUM ):
+                #If spot is free and coin flip is ok and I can fit rooms then I generate a new node
+                if(branch is None and random.randint(1, 3) != 1 and count < MAX_ROOM_NUM ):
+                    #Selecting index (L,U,R,D order)
                     if(index == 0):
                         id = (node.id[0] - 1, node.id[1])
                     elif(index == 1):
@@ -34,38 +40,43 @@ def generate(nodes, depth, count):
                         id = (node.id[0] + 1, node.id[1])
                     elif(index == 3):
                         id = (node.id[0], node.id[1] - 1)
-
+                    #Generating node
                     new_room = Room(ROOM_W, ROOM_H,
                                     (random.randint(0,255), 
                                     random.randint(0,255), 
                                     random.randint(0,255)),
                                     [None]*MAX_ROOM_NEIGH,
-                                    id)
+                                    id,
+                                    node)
+                    #Adding it to the parent
                     node.neighbors[index] = new_room
+                    #Adding it to the tmp list
                     new_node_list.append(new_room)
                     count += 1
         depth += 1
+        #Recurse!
         generate(new_node_list, depth, count)
-"""
-def traverseAndPrint(node, depth, count):
+
+"""def traverseAndPrint(node, depth, count):
     if(count == MAX_ROOM_NUM):
         return
     else:
         for n in node:
+            if(depth == 0):
+                print(n, n.parent)
             for k in n.neighbors:
                 if k is not None:
-                    print('_' * depth, k.name)
+                    print('_' * depth, k, k.parent.id)
                     depth += 1
                     count += 1
                     traverseAndPrint([k], depth ,count)
-"""
 
-"""root = Room(ROOM_W, ROOM_H, (random.randrange(0,255), 
+root = Room(ROOM_W, ROOM_H, (random.randrange(0,255), 
                             random.randrange(0,255), 
                             random.randrange(0,255)), 
                             [None]*MAX_ROOM_NEIGH, 
-                            SCREEN_WIDTH // 2, 
-                            SCREEN_HEIGHT // 2)
- """                       
-#generate([root], 0, 0)
-#traverseAndPrint([root], 0, 1)
+                            (0,0),
+                            None)
+                        
+generate([root], 0, 0)
+traverseAndPrint([root], 0, 1)"""
