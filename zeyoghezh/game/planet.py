01f7f892c1ba812e8cd1c21d635ce9b080baa843
Maku <makusu2@gmail.com>
2020-04-20 16:50:05 -0400

    Improve game over message

--- zeyoghezh/game/planet.py
+++ zeyoghezh/game/planet.py
@@ -116,7 +116,7 @@ class Planet(arcade.Sprite):
             logger.info(f"Removing {self.name} from others in {planet.name}")
             planet.others.remove(self)
         self.parent.planets.remove(self)
-        self.parent.game_over(f"{self.name} has died")
+        self.parent.game_over(f"You cannot leave now; {self.name} has died.")
 
     def get_stats_str(self):
         total_damage_on_others = round(sum(self.damage_on_others.values()), 4)
