d3cb9d3e89123e281fe39c5dd4850978ea2cdab9
sachapomme <sacha02.rolle@gmail.com>
2020-04-20 14:22:12 +0200

    fixed motion, 80% satisfied with it

--- beanoculars/submission/motion.py
+++ beanoculars/submission/motion.py
@@ -4,7 +4,6 @@ from submission.gameConstants import *
 
 
 def movePlayer(sprite: arcade.sprite, dt):
-    print(sprite.destination)
     if sprite.destination != [-1, -1]:
 
         vector = [sprite.destination[0] - math.floor(sprite.center_x - 16) / 32,
@@ -13,23 +12,23 @@ def movePlayer(sprite: arcade.sprite, dt):
         if sprite.is_moving:
             if sprite.direction == 0:
                 if vector[0] > 0:
-                    sprite.center_x += MOVE_SPEED * dt
+                    sprite.center_x += sprite.cur_speed * dt
                     sprite.character_face_direction = LEFT_FACING
                 elif vector[0] < 0:
                     if not sprite.corr_x:
                         vector[0] -= 1
                         sprite.corr_x = True
-                    sprite.center_x -= MOVE_SPEED * dt
+                    sprite.center_x -= sprite.cur_speed * dt
                     sprite.character_face_direction = RIGHT_FACING
 
             elif sprite.direction == 1:
                 if vector[1] > 0:
-                    sprite.center_y += MOVE_SPEED * dt
+                    sprite.center_y += sprite.cur_speed * dt
                 elif vector[1] < 0:
                     if not sprite.corr_y:
                         vector[1] -= 1
                         sprite.corr_y = True
-                    sprite.center_y -= MOVE_SPEED * dt
+                    sprite.center_y -= sprite.cur_speed * dt
 
             if sprite.direction == 0:
 
@@ -43,7 +42,8 @@ def movePlayer(sprite: arcade.sprite, dt):
                     sprite.center_y = sprite.destination[1] * 32 + 16
                     sprite.is_moving = False
 
-            if sprite.center_x == sprite.destination[0] * 32 + 16 and sprite.center_y == sprite.destination[1] * 32 + 16:
+            if sprite.center_x == sprite.destination[0] * 32 + 16 and sprite.center_y == sprite.destination[
+                1] * 32 + 16:
                 sprite.destination = [-1, -1]
 
         else:
@@ -54,7 +54,11 @@ def movePlayer(sprite: arcade.sprite, dt):
                 sprite.direction = 0
             elif abs(vector[0]) < abs(vector[1]):
                 sprite.direction = 1
-            elif vector != [0,0]:
+            elif vector != [0, 0]:
                 sprite.direction = 0
 
             sprite.is_moving = True
+
+
+def updateActualPos(sprite: arcade.sprite):
+    sprite.actual_cur_pos = [math.floor((sprite.center_x - 16) / 32), math.floor((sprite.center_y - 16) / 32)]
