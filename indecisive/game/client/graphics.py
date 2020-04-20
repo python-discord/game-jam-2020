f9b1997a52ae94c5f372caebfda4310e5ea607c8
xedre <git2@xed.re>
2020-04-20 20:16:44 +0100

    resolved Options.png overlap

--- indecisive/game/client/graphics.py
+++ indecisive/game/client/graphics.py
@@ -8,7 +8,7 @@ class Display(arcade.Window):
         super().__init__(1280, 720, "Three of a king")
         self.title = arcade.sprite_list
         arcade.set_background_color((255, 255, 255))
-        self.scene = "mainMenu"
+        self.scene = "loading"
         self.scenes = dict()
 
     def setup(self):
