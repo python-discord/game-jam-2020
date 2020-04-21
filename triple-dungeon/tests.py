1c3a537ad147d29bc7211bd59f8e45bc0eeee941
Xevion <xevioni@yandex.com>
2020-04-20 22:14:24 -0500

    basic class skeleton for tests file

---
+++ triple-dungeon/tests.py
@@ -0,0 +1,31 @@
+"""
+tests.py
+A file dedicated to testing our game and ensuring it can run.
+Integrate this into your IDE's workflow to ensure the game runs from top to bottom.
+The tests used here should test all of our game's features as best they can.
+"""
+
+
+class GameTests:
+    """
+    Tests that the Arcade framework runs the game correctly.
+    Only tests that it launches and runs for a little bit, not that it is functioning properly.
+    """
+
+
+class SpriteTests:
+    """
+    Tests the Sprite classes as well as the sprites, checking that names are correct.
+    """
+
+
+class LevelTests:
+    """
+    Tests the Level class for proper integration.
+    """
+
+
+class DungeonTests:
+    """
+    Tests the Dungeon class for
+    """
