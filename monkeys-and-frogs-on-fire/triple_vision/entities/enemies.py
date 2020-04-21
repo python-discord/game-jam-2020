6393fc1626be555cc6371fe5f8fc9e558eb8cb15
Den4200 <dpham.42@hotmail.com>
2020-04-20 23:12:54 -0400

    randomly raise and lower spikes, dramatically increasing performance

--- monkeys-and-frogs-on-fire/triple_vision/entities/enemies.py
+++ monkeys-and-frogs-on-fire/triple_vision/entities/enemies.py
@@ -1,5 +1,6 @@
 import enum
 from pathlib import Path
+import random
 
 import arcade
 
@@ -26,8 +27,27 @@ class Spike(AnimatedEntity):
             sprite_name='floor_spikes',
             assets_path='assets/dungeon/frames',
             states=('',),
+            frame_range=(0, 1, 2, 3, 0),
             **kwargs
         )
+        self.ticks = 5
+        self.wait_time = random.randrange(2, 10)
+        self.waited_time = 0
+
+    def on_update(self, delta_time: float = 1/60) -> None:
+        if self.ticks == 5:
+            self.waited_time += delta_time
+
+            if self.waited_time >= self.wait_time:
+                self.ticks = 0
+                self.wait_time = random.randrange(2, 10)
+                self.waited_time = 0
+
+        else:
+            if self._prev_anim_delta + delta_time > 0.15:
+                self.ticks += 1
+
+            super().on_update(delta_time)
 
 
 class BaseEnemy(LivingEntity):
