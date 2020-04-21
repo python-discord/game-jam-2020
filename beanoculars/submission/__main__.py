ddf9b6c7250571783afd3a8f4aa15eceac6a848c
sachapomme <sacha02.rolle@gmail.com>
2020-04-21 15:16:29 +0200

    first game mode commit

--- beanoculars/submission/__main__.py
+++ beanoculars/submission/__main__.py
@@ -10,10 +10,11 @@ Author: sachapomme (or Sacha#8175)
 import arcade
 
 from submission.game import MyGame
-from submission.gameConstants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE
+from submission.gameConstants import WINDOW_HEIGHT, WINDOW_WIDTH, WINDOW_TITLE, GM_MENU, GM_GAME
 
 def main() -> None:
     """ Main method """
+    gameMode = GM_GAME
     window = MyGame(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
     print('Window created')
     window.setup()
