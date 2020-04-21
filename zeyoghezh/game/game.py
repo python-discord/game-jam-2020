dc9291264aab80ce9c531bda18547a341ab1730d
Maku <makusu2@gmail.com>
2020-04-21 19:36:55 -0400

    Simplify banner

--- zeyoghezh/game/game.py
+++ zeyoghezh/game/game.py
@@ -45,7 +45,7 @@ class Game(arcade.Window):
         self.player_has_clicked_lithium = False
         self.player_has_healed_planet = False
         self.banner_text = None
-        self.banner_location = (SCREEN_SIZE[0]/10, SCREEN_SIZE[1]/2)
+        self.banner_location = (SCREEN_SIZE[0]/100, SCREEN_SIZE[1]/2)
         self.last_banner_change = None
         self.story_iter = None
 
@@ -80,6 +80,18 @@ class Game(arcade.Window):
                 start_speed_x=random.random(),
                 start_speed_y=random.random()
             )
+
+    def set_banner_text(self, new_text):
+        if new_text == self.banner_text:
+            return
+        self.last_banner_change = time.time()
+        self.banner_text = new_text
+        x_coord = SCREEN_SIZE[0]/100
+        y_coord = (
+            (random.random() * (SCREEN_SIZE[1] / 8))
+            + (SCREEN_SIZE[1] / 5)
+        )
+        self.banner_location = (x_coord, y_coord)
 
     def abscond_press(self):
         self.abscond_button.pressed = True
@@ -189,25 +201,22 @@ class Game(arcade.Window):
             return
         now = time.time()
         if self.last_banner_change is None:
-            self.banner_text = "This is the story of Ze, Yogh, and Ezh."
-            self.last_banner_change = now
+            self.set_banner_text("This is the story of Ze, Yogh, and Ezh.")
         delta_time = now - self.last_banner_change
         if not self.player_has_clicked_lithium and delta_time > 3:
-            self.banner_text = "See the circles? Click on their intersection."
-            self.last_banner_change = now
+            self.set_banner_text(
+                "See the circles? Click on their intersection.")
         if (self.player_has_clicked_lithium
                 and not self.player_has_healed_planet
                 and not self.lithium_count > 2
                 and delta_time > 0.5):
-            self.banner_text = "Good. Keep doing it."
-            self.last_banner_change = now
+            self.set_banner_text("Good. Keep doing it.")
         if self.lithium_count > 2 and not self.player_has_healed_planet:
-            self.banner_text = (
+            self.set_banner_text(
                 "Good. Now heal one of the planets by clicking on them."
             )
-            self.last_banner_change = now
         if self.player_has_healed_planet and delta_time < 1:
-            self.banner_text = (
+            self.set_banner_text(
                 "You've healed them with lithium. Keep them alive."
             )
         if self.player_has_healed_planet and delta_time > 1:
@@ -219,8 +228,7 @@ class Game(arcade.Window):
         if delta_time < 3:
             return
         next_story_part = next(self.story_iter, self.banner_text)
-        self.banner_text = next_story_part
-        self.last_banner_change = now
+        self.set_banner_text(next_story_part)
 
     def run_assertions(self):
         assert len(self.planets) in (1, 2, 3)
