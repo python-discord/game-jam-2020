e8ef159364dc9b69c27c88cfeb531986ad6ccc15
Maku <makusu2@gmail.com>
2020-04-22 19:55:28 -0400

    Buff push

--- zeyoghezh/game/config.py
+++ zeyoghezh/game/config.py
@@ -8,7 +8,7 @@ LITHIUM_MULTIPLIER = 1.8
 
 BASE_SPEED = 1e+1  # TODO change to 1e-1 when this is over
 PLANET_BASE_SPEED = 5 * BASE_SPEED
-PUSH_BASE_SPEED = 3 * BASE_SPEED
+PUSH_BASE_SPEED = 4 * BASE_SPEED
 PUSH_MAX_DISTANCE = 80  # The most a planet can be away from Yogh to be pushed
 BASE_DAMAGE = 1e-4  # TODO change to 1e-4 when this is over
 PLANET_DAMAGE = {
