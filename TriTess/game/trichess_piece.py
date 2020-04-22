TriPieces now inherit from arcade sprite - added images for different chess pieces in data directory - added correct rotation for the pieces depending on who the player is - added scaling to pieces based on board size

TriTess/game/trichess_piece.py
@@ -1,3 +1,5 @@
+import os
+
 import arcade
 import numpy as np
 
@@ -5,116 +7,73 @@ PLAYER1_COLOR = arcade.color.IVORY
 PLAYER2_COLOR = arcade.color.BLACK
 PLAYER3_COLOR = arcade.color.BATTLESHIP_GREY
 color_dict = {0: PLAYER1_COLOR, 1: PLAYER2_COLOR, 2: PLAYER3_COLOR}
+data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)).rsplit(os.sep, 0)[0], 'data')
 
 
-class TriPiece:
-    def __init__(self, pos, player, cell_width):
+class TriPiece(arcade.Sprite):
+    def __init__(self, pos, player, image, trigrid):
+        self.trigrid = trigrid
+        super().__init__(image, self.trigrid.cell_width / 6)
+        if player == 1:
+            self.angle += 120
+        elif player == 2:
+            self.angle += 240
+
         self.pos = pos
+        self.center_x, self.center_y = self.get_coord_from_pos(*self.pos)
         self.player = player
-        self.cell_width = cell_width
 
-    def get_shape(self):
+    def get_coord_from_pos(self, x, y, r):
+        x, y = self.trigrid.get_grid_position(x, y, r)
+        return x, y
+
+    def list_valid_moves(self, grid):
+        return []
+
+    def list_valid_moves(self, grid):
         pass
-
-    def moves(self, grid):
-        pass
-
-    def attack(self, grid):
-        pass
-
-    def calc_cell_coord(self, x, y, r):
-        x_offset, y_offest = self.calc_face_offset(x, y, r)
-        rot_mat = self.calc_rot()
-
-        piece_coord = self.get_shape()
-        piece_coord = piece_coord * self.cell_width
-        piece_coord = piece_coord * rot_mat
-        piece_coord[:, 0] += x_offset
-        piece_coord[:, 1] += y_offest
-
-        return list(zip(piece_coord[:, 0], piece_coord[:, 1]))
-
-    #TODO calc offset
-    def calc_face_offset(self, x, y, r):
-        return 0, 0
-
-    def piece_poly(self):
-        coord = self.calc_cell_coord(*self.pos)
-
-        arcade.create_polygon(coord, color_dict[self.player])
-
-    def calc_rot(self):
-        if self.player == 0:
-            rot_mat = np.array([1, 0], [0, 1])
-        elif self.player == 1:
-            rot_mat = np.array([np.cos(np.pi * (210 / 180)), -np.sin(np.pi * (210 / 180))],
-                               [np.sin(np.pi * (210 / 180)), np.cos(np.pi * (210 / 180))])
-        elif self.player == 2:
-            rot_mat = np.array([np.cos(np.pi * (330 / 180)), -np.sin(np.pi * (330 / 180))],
-                               [np.sin(np.pi * (330 / 180)), np.cos(np.pi * (330 / 180))])
-        return rot_mat
 
 
 class Pawn(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_pawn{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
-    def get_shape_coord(self):
-        coord = ()
-
-        return coord
+    def list_valid_moves(self, grid):
+        return []
 
 
 class Rook(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_rook{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
 
 class Bishop(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_bishop{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
 
 class UKnight(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_uknight{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
 
 class Knight(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_knight{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
 
 class Queen(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_queen{player}.png')
+        super().__init__(pos, player, sprite_image, scale)
 
 
 class King(TriPiece):
-    def __init__(self, pos, player):
-        super().__init__(pos, player)
-        self.shape = self.get_shape()
-
-    def get_shape(self):
-        return 1
+    def __init__(self, pos, player, scale=5):
+        sprite_image = os.path.join(data_dir, f'sprite_king{player}.png')
+        super().__init__(pos, player, sprite_image, scale)