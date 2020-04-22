b2b86b6b5c6c7133eba7e56a1a1b36206a0818c1
world-wake <47750505+world-wake@users.noreply.github.com>
2020-04-22 16:39:00 +0300

    Implement function get_puzzle_clues with list interface.

--- the-comet-that-shook-the-py/backend.py
+++ the-comet-that-shook-the-py/backend.py
@@ -32,28 +32,7 @@ def get_new_game():
 
 
 def get_puzzle_clues(solution_list, fact_templates):
-    # example_fact_template =
-    #     ["{0} live next to the person who likes {4}",
-    #      "The person in the {8} house likes {5}",
-    #      "The person who likes {4} lives in the {7} house",
-    #      "The person who likes {3} doesn't live next to the person who likes {5}",
-    #      "{2}'s house is {8}",
-    #      "{0} lives to the left of the {8} house."]
-    # example_solution =
-    #     ['Tom','Dick','Harry','Grapes','Cherries','Blueberries','Red',
-    #      'White','Blue']
-    # should result in
-    #
-    # ["Tom live next to the person who likes cherries",
-    #  "The person in the blue house likes blueberries",
-    #  "The person who likes cherries lives in the white house",
-    #  "The person who likes grapes doesn't live next to the person who likes bluberries",
-    #  "Harry's house is blue"]
-    #
-    #
-    # apply solution_list elements to fact_templates
-    # clue_list = applied solution_list to fact_templates
-    return clue_list
+    return [fact.format(*solution_list) for fact in fact_templates]
 
 
 def get_fact_template():
@@ -72,18 +51,19 @@ def start_new_game():
     shuffled_list = solution_list[:]
     random.shuffle(shuffled_list)
     fact_templates = get_fact_template()
-    clue_list = get_puzzle_clues(solution_list, fact_templates)
     # real return value
     # return solution_list, shuffled_list, clue_list
 
-    # bogus test code follows:
-    solution_list = ['Tom','Dick','Harry','Grapes','Cherries','Blueberries','Red', 'White','Blue']
-    clue_list = ["Tom live next to the person who likes cherries",
+    clue_list = get_puzzle_clues(solution_list, fact_templates)
+
+    return solution_list, shuffled_list, clue_list
+
+
+# bogus test code follows:
+# solution_list_ = ['Tom','Dick','Harry','Grapes','Cherries','Blueberries','Red', 'White','Blue']
+"""    clue_list_ = ["Tom live next to the person who likes cherries",
                  "The person in the blue house likes blueberries",
                  "The person who likes cherries lives in the white house",
                  "The person who likes grapes doesn't live next to the person who likes bluberries",
                  "Harry's house is blue"]
-    shuffled_list = solution_list[:]
-    random.shuffle(shuffled_list)
-
-    return solution_list, shuffled_list, clue_list
+"""
