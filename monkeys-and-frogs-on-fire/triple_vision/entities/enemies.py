3c167e05d62fac14bd6dd2e90daab0fc0d00a918
Braindead <albertopoljak@gmail.com>
2020-04-22 23:36:01 +0200

    Added back simple chasing enemy.

--- monkeys-and-frogs-on-fire/triple_vision/entities/enemies.py
+++ monkeys-and-frogs-on-fire/triple_vision/entities/enemies.py
@@ -36,10 +36,45 @@ class BaseEnemy(LivingEntity):
         self.being_pushed = False
 
 
-class ChasingEnemy(BaseEnemy, MovingSprite):
+class SimpleChasingEnemy(BaseEnemy, MovingSprite):
     """
     Simple chasing enemy that tries to catch some other sprite.
     No path-finding, just goes straight to sprite if it is in radius.
+    """
+
+    def __init__(
+        self,
+        enemy: Enemies,
+        target_sprite: arcade.Sprite,
+        detection_radius: int,
+        **kwargs
+    ) -> None:
+        super().__init__(enemy, rotate=False, **kwargs)
+
+        self.target_sprite = target_sprite
+        self.detection_radius = detection_radius
+
+    def on_update(self, delta_time: float = 1/60) -> None:
+        if not self.being_pushed:
+            if is_in_radius(self, self.target_sprite, self.detection_radius):
+                self.move_to(
+                    self.target_sprite.center_x,
+                    self.target_sprite.center_y,
+                )
+            else:
+                self.change_x = 0
+                self.change_y = 0
+
+        # Since both are defined in both parents it's gonna call only from BaseEnemy
+        # so we're forcing the call for MovingSprite
+        super().on_update(delta_time)
+        super().force_moving_sprite_on_update(delta_time)
+
+
+class ChasingEnemy(BaseEnemy, MovingSprite):
+    """
+    Advanced chasing enemy that, if in detection radius, tries to catch some other sprite.
+    Uses path-finding.
     """
 
     def __init__(
