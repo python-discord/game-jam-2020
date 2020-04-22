98bfcccc44160716de24acf51b167d2d27f1ff46
F4zii <fazbosoreq@gmail.com>
2020-04-22 15:59:19 +0300

    Move SoundManager to managers.py

--- monkeys-and-frogs-on-fire/triple_vision/managers.py
+++ monkeys-and-frogs-on-fire/triple_vision/managers.py
@@ -1,9 +1,11 @@
-from typing import Optional, Tuple
+import itertools
+from typing import Optional, Tuple, List
 
 import arcade
 
-from triple_vision import Settings as s
+from triple_vision import Settings as s, SoundSettings as ss
 from triple_vision.entities import DamageIndicator
+from triple_vision.sound import Sound
 
 
 class GameManager:
@@ -175,3 +177,71 @@ class CardManager:
                 card.change_y = -10
 
         self.cards.update()
+
+
+class SoundManager:
+    def __init__(self, sounds: List[arcade.Sound] = []) -> None:
+        self._sounds = sounds
+        self._sounds_cycle = itertools.cycle(self._sounds)
+        self.curr_sound: Sound = None
+        self.tick_delta = 0.0
+        self.playing = False
+
+    def add_sound(self, sound_name: str, faded: bool = False, max_volume: float = 1.0) -> None:
+        self._sounds.append(Sound(sound_name, is_faded=faded, max_volume=max_volume))
+        self.update_cycle()
+
+    def remove_sound(self, sound_name: str):
+        self._sounds = [sound for sound in self._sounds if sound.file_name != sound_name]
+        self.update_cycle()
+
+    def play_external_sound(
+        self, sound_name: str = None, faded: bool = False, max_volume: float = 1.0
+    ) -> None:
+        self.curr_sound = Sound(sound_name, is_faded=faded, max_volume=max_volume)
+        self.playing = False
+
+    def play_sound_from_list(self, index):
+        self.curr_sound = (
+            self._sounds[index]
+            if index - len(self._sounds) <= index < len(self._sounds)
+            else self.curr_sound
+        )
+        self.playing = False
+
+    def toggle_next_sound(self) -> None:
+        self.curr_sound = next(self._sounds_cycle)
+
+    def update_cycle(self):
+        self._sounds_cycle = itertools.cycle(self._sounds)
+
+    @staticmethod
+    def load_sound(
+        file_name: str, is_faded: bool = False, max_volume: float = 1.0
+    ) -> Optional[Sound]:
+
+        try:
+            sound = Sound(file_name, is_faded=is_faded, max_volume=max_volume)
+            return sound
+        except Exception as e:
+            print(f'Unable to load sound file: "{file_name}". Exception: {e}')
+            return None
+
+    def update(self, delta_time: float) -> None:
+        self.tick_delta += delta_time
+
+        if self.curr_sound is None:
+            return
+
+        if not self.playing:
+            arcade.play_sound(self.curr_sound)
+            self.playing = True
+
+        if not self.curr_sound.faded:
+            return
+
+        if self.tick_delta < ss.FADE_FREQUENCY:
+            return
+
+        self.curr_sound.set_volume(self.curr_sound.get_volume() + ss.FADE_AMOUNT)
+        self.tick_delta = 0.0
