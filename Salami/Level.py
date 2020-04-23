d03c6aedc681251a70b992933a49a0b23b20f903
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-24 07:59:19 +0800

    Added level gen

--- Salami/Level.py
+++ Salami/Level.py
@@ -10,13 +10,15 @@ import Maths
 from Textures import Textures
 
 from Engine import Engine
-from Entity import Entity, Texs
+from Entity import Entity, Texs, Tex
+from Projectile import Projectile
 from Mob import Mob
 from Ball import Ball
 from Tile import Tile
 
-from Constants import WIDTH, HEIGHT, TILE_SIZE, GRAVITY, SQUARE_HIT_BOX
-
+from Constants import WIDTH, HEIGHT, \
+    TILE_SIZE, GRAVITY, SQUARE_HIT_BOX, \
+    ROOM_WIDTH, ROOM_HEIGHT
 
 class Level:
 
@@ -32,15 +34,13 @@ class Level:
         self.width = WIDTH // TILE_SIZE
         self.height = HEIGHT // TILE_SIZE
 
-        self.movespeed = 2
+        self.movespeed = 1
         self.jump_height = 4
         self.jumping = False
 
         self.curr_jump_height = 0
-        self.min_jump_height = 16
-        self.max_jump_height = 32
-
-        LevelGenerator.generateLevel(self)
+        self.min_jump_height = 8
+        self.max_jump_height = 64
 
         # for i in range(100):
         #     ball = Ball(Texs.BALL, 128 + 128 * random.random(), 128 + 128 * random.random())
@@ -54,19 +54,26 @@ class Level:
         self.add_entity_to_list(self.enemy, self.entities)
         self.enemy.flying = True
 
-        self.idle_texture = 0
-
-        self.idle_texture = arcade.load_texture("Salami/spritesheet.png", 0, TILE_SIZE * 3, TILE_SIZE, TILE_SIZE)
-        self.idle_texture_mirrored = arcade.load_texture("Salami/spritesheet.png", 0, TILE_SIZE * 3, TILE_SIZE, TILE_SIZE, mirrored=True)
-        self.walking_textures = Texs.load_textures("Salami/spritesheet.png", 0, 3, TILE_SIZE, 6)
-        self.walking_textures_mirrored = Texs.load_textures("Salami/spritesheet.png", 0, 3, TILE_SIZE, 6, mirrored=True)
+        self.idle_texture = arcade.load_texture("Salami/spritesheet.png", TILE_SIZE * 4, TILE_SIZE * 2, TILE_SIZE, TILE_SIZE)
+        self.idle_texture_mirrored = arcade.load_texture("Salami/spritesheet.png", TILE_SIZE * 4, TILE_SIZE * 2, TILE_SIZE, TILE_SIZE, mirrored=True)
+        self.walking_textures = Texs.load_textures("Salami/spritesheet.png", 4, 2, TILE_SIZE, 3)
+        self.walking_textures_mirrored = Texs.load_textures("Salami/spritesheet.png", 4, 2, TILE_SIZE, 3, mirrored=True)
 
         self.walk_count = 0
-        self.walk_frame_speed = 5
+        self.walk_frame_speed = 12
         self.player_dir = True
 
         self.player = Mob(Texs.BALL, 64, 64)
         self.add_entity_to_list(self.player, self.entities)
+
+        level_gen_x = self.player.center_x // TILE_SIZE // ROOM_WIDTH
+        level_gen_y = self.player.center_y // TILE_SIZE // ROOM_HEIGHT
+
+        LevelGenerator.generateLevel(self, int(level_gen_x), int(level_gen_y))
+        
+        self.ball = Ball(Texs.BALL, self.player.center_x, self.player.center_y)
+        self.ball.change_x = 2
+        self.add_entity_to_list(self.ball, self.entities)
 
         self.engine = Engine(self.entities, self.tile_list, GRAVITY)
         
@@ -75,6 +82,18 @@ class Level:
         self.count = 0
 
     def update(self, delta):
+
+        if self.keyboard.is_pressed("dash"):
+            level_gen_x = self.player.center_x // TILE_SIZE // ROOM_WIDTH
+            level_gen_y = self.player.center_y // TILE_SIZE // ROOM_HEIGHT
+
+            LevelGenerator.generateLevel(self, int(level_gen_x), int(level_gen_y))
+
+        if self.keyboard.is_pressed("attack"):
+            self.ball.center_x = self.player.center_x
+            self.ball.center_y = self.player.center_y
+            self.ball.change_x = self.player.change_x * 8
+            self.ball.change_y = self.player.change_y * 8
 
         if self.keyboard.is_pressed("jump"):
             if self.physics_engine.can_jump(1):
@@ -96,10 +115,14 @@ class Level:
             self.player.change_y = self.jump_height
             self.curr_jump_height += self.jump_height
 
+        speed_mult = 3
+        if self.keyboard.is_pressed("sprint"):
+            speed_mult = 1
+
         if self.keyboard.is_pressed("left"):
-            self.player.change_x = -self.movespeed
+            self.player.change_x = -self.movespeed * speed_mult
         elif self.keyboard.is_pressed("right"):
-            self.player.change_x = self.movespeed
+            self.player.change_x = self.movespeed * speed_mult
         else:
             if self.player.change_x > 0:
                 self.player.change_x -= 1
@@ -110,25 +133,20 @@ class Level:
         if self.walk_count >= len(self.walking_textures) * self.walk_frame_speed:     
             self.walk_count = 0
         if self.player.change_x > 0:
-            self.player.texture = self.walking_textures_mirrored[self.walk_count // self.walk_frame_speed]
+            self.player.texture = self.walking_textures[self.walk_count // self.walk_frame_speed]
             self.player_dir = True
 
         elif self.player.change_x < 0:
             frame = self.walk_count // self.walk_frame_speed
-            self.player.texture = self.walking_textures[frame]
-            # self.player.texture = self.walking_textures_mirrored[self.walk_count // self.walk_frame_speed]
+            self.player.texture = self.walking_textures_mirrored[frame]
             self.player_dir = False
         else:
             if self.player_dir:
-                self.player.texture = self.idle_texture_mirrored
-            else:
                 self.player.texture = self.idle_texture
+            else:
+                self.player.texture = self.idle_texture_mirrored
 
         self.engine.update(delta)
-
-        if self.count % 30 == 0:
-            tile = Tile(Texs.ROCK_TILE, random.randint(0, 49) * TILE_SIZE, random.randint(0, 49) * TILE_SIZE)
-            self.add_entity_to_list(tile, self.tile_list)
 
         self.count += 1
 
@@ -152,6 +170,8 @@ class Level:
     def set_tile(self, x, y, tile):
         if x < 0 or y < 0 or x >= self.width or y >= self.height:
             return
+        if self.tiles[(x, y)]:
+            self.tile_list.remove(self.tiles[(x, y)])
         self.tile_list.append(tile)
         self.tiles[(x, y)] = tile
 
--- Salami/LevelGenerator.py
+++ Salami/LevelGenerator.py
@@ -1,33 +1,87 @@
 
+import random
+
 from Tile import Tile
-from Entity import Texs
+from Entity import Texs, Tex
 
-from Constants import TILE_SIZE
-
-ROOM_WIDTH = 15
-ROOM_HEIGHT = 12
+from Constants import TILE_SIZE, ROOM_WIDTH, ROOM_HEIGHT
 
 LEVEL_WIDTH = 60
 LEVEL_HEIGHT = 36
 
-def generateLevel(level):
-    generateRoom(level, 0, 0)
-    generateRoom(level, ROOM_WIDTH + 1, 0)
-    generateRoom(level, 0, ROOM_HEIGHT + 1)
-    generateRoom(level, ROOM_WIDTH + 1, ROOM_HEIGHT + 1)
+LEVEL_DEPTH = 8
+
+MAX_TRIES = 8
+
+DIRS = [
+    [-1, 0], [0, -1], [1, 0], [0, 1]
+]
+
+# DIRS = [
+#     [-1, 1] , [0, 1] , [1, 1],
+#     [-1, 0] ,          [1, 0],
+#     [-1, -1], [0, -1], [1, -1]
+# ]
+
+class Room:
+    def __init__(self, x, y, direction, prev_room):
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.prev_room = prev_room
+
+def generateLevel(level, x: int, y: int):
+    rooms = [(x, y)]
+    depth = 0
+    prev_index = 0
+    prev_room = (x, y)
+    curr_room = (x, y)
+    tries = 0
+    while depth < LEVEL_DEPTH:
+        rand_dir = random.randint(0, len(DIRS) - 1)
+        direction = DIRS[rand_dir]
+        curr_room = (prev_room[0] + direction[0], prev_room[1] + direction[1])
+        if curr_room not in rooms:
+            rooms.append(curr_room)
+            prev_room = curr_room
+            prev_index = len(rooms) - 1
+            depth += 1
+        else:
+            if prev_index >= 0:
+                prev_index -= 1
+                prev_room = rooms[prev_index]
+            else:
+                tries += 1
+                if tries > MAX_TRIES:
+                    break
+
+    for room in rooms:
+        generateRoom(level, room[0] * ROOM_WIDTH, room[1] * ROOM_HEIGHT)
+
+    print(rooms)
+
+    # generateRoom(level, 0, 0)
+    # generateRoom(level, ROOM_WIDTH + 1, 0)
+    # generateRoom(level, 0, ROOM_HEIGHT + 1)
+    # generateRoom(level, ROOM_WIDTH + 1, ROOM_HEIGHT + 1)
+
+    # drawLine(level, 0, -1, LEVEL_WIDTH, -1, Tex("Salami/spritesheet.png", 0, 0, TILE_SIZE))
+    # drawLine(level, -1, 0, -1, LEVEL_HEIGHT, Tex("Salami/spritesheet.png", 0, 0, TILE_SIZE))
 
 def generateRoom(level, x, y):
-    drawLine(level, x, y, x + ROOM_WIDTH, y)
-    drawLine(level, x + ROOM_WIDTH, y, x + ROOM_WIDTH, y + ROOM_HEIGHT)
-    drawLine(level, x, y, x, y + ROOM_HEIGHT)
+    # drawLine(level, x, y + ROOM_HEIGHT, x, y + ROOM_HEIGHT, Texs.ROCK_TILE)
+    drawLine(level, x, y, x + ROOM_WIDTH, y, Tex("Salami/spritesheet.png", 0, 0, TILE_SIZE))
+    # drawLine(level, x + ROOM_WIDTH, y, x + ROOM_WIDTH, y + ROOM_HEIGHT, Texs.ROCK_TILE)
+    # drawLine(level, x, y, x, y + ROOM_HEIGHT, Texs.ROCK_TILE)
+    # drawLine(level, x, y + ROOM_HEIGHT, x + ROOM_WIDTH, y + ROOM_HEIGHT, Texs.ROCK_TILE)
 
-def drawLine(level, x0, y0, x1, y1):
-    for x in range(x0 - 1, x1):
-        tile = Tile(Texs.ROCK_TILE, x * TILE_SIZE, y0 * TILE_SIZE)
+def drawLine(level, x0, y0, x1, y1, tex):
+    for x in range(x0, x1):
+        tile = Tile(tex, x * TILE_SIZE, y0 * TILE_SIZE)
         level.add_entity_to_list(tile, level.tile_list)
 
-    for y in range(y0 - 1, y1):
-        tile = Tile(Texs.ROCK_TILE, x0 * TILE_SIZE, y * TILE_SIZE)
+    for y in range(y0, y1):
+        tile = Tile(tex, x0 * TILE_SIZE, y * TILE_SIZE)
         level.add_entity_to_list(tile, level.tile_list)
 
 def drawRect(level, x0, y0, x1, y1):
