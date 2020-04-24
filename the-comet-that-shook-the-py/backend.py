284c2342599cb9a2e5b225ca93b759789aba4e2b
Bradley M. Small <bms@bmsPC.bradleysmall.com>
2020-04-24 02:30:53 -0400

    updating backend for design and lint fixes.
    Broken unit tests commented. Random moved to get_count functions.
    Underscores added to all functions not meant for public API. I
    attempted to correct unit tests but it was too late at night to
    figure out their actual intent relative to the changes made.

--- the-comet-that-shook-the-py/backend.py
+++ the-comet-that-shook-the-py/backend.py
@@ -1,77 +1,93 @@
+"""Backend API module."""
+
+
 import random
+import os
 from pathlib import Path
 from typing import List
 
 
-def _get_count_files_starting_with(prefix: str) -> List[str]:
-    """
+def _get_count_files_starting_with(prefix: str, count=3) -> List[str]:
+    """Retrieve 'count' random files starting with the given prefix.
+
     :param prefix: the prefix to use to retrieve assets
     :return: a list of the relative filenames
     """
     asset_path = Path('assets')
     all_files = asset_path.rglob(f'{prefix}_*')
     all_row_items = [file.name for file in all_files]
-    return all_row_items
+    return random.sample(all_row_items, count)
 
 
-def get_r1_attrs(count=3) -> List[str]:
-    attrs = _get_count_files_starting_with("R1")
-    return random.sample(attrs, count)
+def _get_r1_attrs(count=3) -> List[str]:
+    """Retrive 'count' randomized first row attributes."""
+    attrs = _get_count_files_starting_with("R1", count)
+    return attrs
 
 
-def get_r2_attrs(count=3) -> List[str]:
-    attrs = _get_count_files_starting_with("R2")
-    return random.sample(attrs, count)
+def _get_r2_attrs(count=3) -> List[str]:
+    """Retrive 'count' randomized second row attributes."""
+    attrs = _get_count_files_starting_with("R2", count)
+    return attrs
 
 
-def get_r3_attrs(count=3) -> List[str]:
-    attrs = _get_count_files_starting_with("R3")
-    return random.sample(attrs, count)
+def _get_r3_attrs(count=3) -> List[str]:
+    """Retrive 'count' randomized third row attributes."""
+    attrs = _get_count_files_starting_with("R3", count)
+    return attrs
 
 
-def get_new_game():
-    solution_list = get_r1_attrs() + get_r2_attrs() + get_r3_attrs()
+def _get_fact_template(filename=None, count=1) -> List[str]:
+    """Retrive 'count' randomized clue template.
+
+    The count should always be 1.
+    """
+    if not filename:
+        filename = _get_count_files_starting_with('f1', count)
+
+    filename = os.path.join('assets', filename[0])
+    with open(filename, 'rt', encoding='utf8') as file:
+        template = file.readlines()
+
+    return template
+
+
+def _get_new_game():
+    """Build a new solution in order to create a new game."""
+    solution_list = _get_r1_attrs() + _get_r2_attrs() + _get_r3_attrs()
     return solution_list
 
-def get_fact_templates(files=None):
 
-    if not files:
-        files = _get_count_files_starting_with('f1')
-
-    templates = []
-
-    for filename in files:
-        with open(f'assets/{filename}', 'rt', encoding='utf8') as file:
-            template_ = [line for line in file.read().splitlines() if line.strip()]
-            templates.append(template_)
-
-    return templates
-
-def get_puzzle_clues(solution_list, fact_templates):
-    if not solution_list or not fact_templates:
+def _get_puzzle_clues(solution_list, fact_template):
+    """Build a working set of clues from a template and a given solution."""
+    if not solution_list or not fact_template:
         raise TypeError("Missing input for function get_puzzle_clues!")
 
-    return [fact.format(*solution_list) for fact in fact_templates]
+    return [fact.format(*[item.split('_')[1] for item in solution_list])
+            for fact in fact_template]
 
 
 def check_results(result, solution_list):
+    """Compare a working solution to an attempted solution."""
     return result == solution_list
 
 
 def start_new_game():
-    solution_list = get_new_game()
+    """Create and return a new game.
+
+    Includes the solution, shuffled list of assets, and clues."""
+    solution_list = _get_new_game()
     shuffled_list = solution_list[:]
     random.shuffle(shuffled_list)
-    fact_templates = get_fact_templates()
-    fact_template = fact_templates[0]
-    # real return value
-    # return solution_list, shuffled_list, clue_list
-    clue_list = get_puzzle_clues(solution_list, fact_template)
+    fact_template = _get_fact_template()
+    clue_list = _get_puzzle_clues(solution_list, fact_template)
 
     return solution_list, shuffled_list, clue_list
 
-a, b, c = start_new_game()
 
-print(a)
-print(b)
-print(c)
+if __name__ == '__main__':
+    A, B, C = start_new_game()
+
+    print(A)
+    print(B)
+    print(C)
