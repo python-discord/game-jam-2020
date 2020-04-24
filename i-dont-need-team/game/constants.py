c64376db229b29afc7de91872d92069b04c6af91
ks129 <45097959+ks129@users.noreply.github.com>
2020-04-24 13:15:42 +0300

    Created main constants
    Added information about blocks size, amount, screen size and title.

---
+++ i-dont-need-team/game/constants.py
@@ -0,0 +1,14 @@
+# Every block that is in board size
+BLOCK_WIDTH = 30
+BLOCK_HEIGHT = 30
+
+# How much blocks in board
+BLOCKS_AMOUNT_Y = 15 + 1 # One here and below is additional frame block
+BLOCKS_AMOUNT_X = 15 + 1
+
+# Calculate screen size based on these stats
+SCREEN_WIDTH = BLOCKS_AMOUNT_Y * BLOCK_WIDTH
+SCREEN_HEIGHT = BLOCKS_AMOUNT_X * BLOCK_HEIGHT
+
+# Title that will be shown on window
+SCREEN_TITLE = "Adventures of 3 Balls"
