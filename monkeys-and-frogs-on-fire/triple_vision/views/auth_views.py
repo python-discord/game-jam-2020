c0df34f9d77b913185341ce518e0d7726b9ec9ae
Den4200 <dpham.42@hotmail.com>
2020-04-24 00:36:59 -0400

    move LoginButton and RegisterButton to auth_views.py

--- monkeys-and-frogs-on-fire/triple_vision/views/auth_views.py
+++ monkeys-and-frogs-on-fire/triple_vision/views/auth_views.py
@@ -2,7 +2,39 @@ import arcade
 
 from triple_vision import Settings as s
 from triple_vision.text_input import TextInput
-from triple_vision.views.buttons import LoginButton, RegisterButton
+
+class LoginButton(arcade.TextButton):
+
+    def __init__(self, view, *args, **kwargs) -> None:
+        super().__init__(text='Login', *args, **kwargs)
+        self.view = view
+
+        self.pressed = False
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        if self.pressed:
+            self.view.login()
+            self.pressed = False
+
+
+class RegisterButton(arcade.TextButton):
+
+    def __init__(self, view, *args, **kwargs) -> None:
+        super().__init__(text='Register', *args, **kwargs)
+        self.view = view
+
+        self.pressed = False
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        if self.pressed:
+            self.view.register()
+            self.pressed = False
 
 
 class AuthTextInput(TextInput):
