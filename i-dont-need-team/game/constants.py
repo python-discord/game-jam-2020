69235ad8344817f5cb85e5600d8712bf3e205473
ks129 <45097959+ks129@users.noreply.github.com>
2020-04-24 18:17:59 +0300

    Added new constants class `Blocks`
    `Blocks` enum hold `int`s of position of textures in (upcoming)
    textures list.

--- i-dont-need-team/game/constants.py
+++ i-dont-need-team/game/constants.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 # Every block that is in board size
 BLOCK_WIDTH = 50
 BLOCK_HEIGHT = 50
@@ -12,3 +14,13 @@ SCREEN_HEIGHT = BLOCKS_AMOUNT_X * BLOCK_HEIGHT
 
 # Title that will be shown on window
 SCREEN_TITLE = "Adventures of 3 Balls"
+
+
+class Blocks(Enum):
+    """All blocks IDs that will be used to set textures and make controls."""
+
+    empty = 0
+    frame = 1
+    wall = 2
+    trap = 3
+    water = 4
