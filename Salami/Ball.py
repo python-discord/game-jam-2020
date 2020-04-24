2c782e5af622684aeed8307bfca769e261dd0f56
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-24 23:11:30 +0800

    Changed bouncing

--- Salami/Ball.py
+++ Salami/Ball.py
@@ -32,29 +32,45 @@ class Ball(Mob):
         # print(f"{self.change_x} {self.change_y}")
 
     def move(self, dx: float, dy: float):
-        
         if dy != 0:
             self.center_y += dy
 
-            collision_list_y = arcade.check_for_collision_with_list(self, self.level.engine.tiles)
+            # collision_list_y = self.level.get_tiles(
+            #     int(self.left / TILE_SIZE),
+            #     int(self.right / TILE_SIZE),
+            #     int(self.bottom / TILE_SIZE),
+            #     int(self.top / TILE_SIZE))
+            collision_list_y = arcade.check_for_collision_with_list(self, self.level.tile_list)
 
             for entity in collision_list_y:
+                if not entity.is_solid:
+                    continue
                 if self.intersects(entity):
                     if self.change_y > 0:
                         self.center_y = entity.center_y - self.width
                     elif self.change_y < 0:
-                        self.center_y = entity.center_y + entity.width
+                        self.center_y = entity.center_y + self.width
                     self.change_y *= -0.8
+                    self.change_x *= 0.8
+
         if dx != 0:
 
             self.center_x += dx
-            collision_list_x = arcade.check_for_collision_with_list(self, self.level.engine.tiles)
+
+            # collision_list_x = self.level.get_tiles(
+            #     int(self.left / TILE_SIZE),
+            #     int(self.right / TILE_SIZE),
+            #     int(self.bottom / TILE_SIZE),
+            #     int(self.top / TILE_SIZE))
+
+            collision_list_x = arcade.check_for_collision_with_list(self, self.level.tile_list)
 
             for entity in collision_list_x:
-
+                if not entity.is_solid:
+                    continue
                 if self.intersects(entity):
                     if self.change_x > 0:
                         self.center_x = entity.center_x - self.width
                     elif self.change_x < 0:
-                        self.center_x = entity.center_x + entity.width
+                        self.center_x = entity.center_x + self.width
                     self.change_x *= -0.6
