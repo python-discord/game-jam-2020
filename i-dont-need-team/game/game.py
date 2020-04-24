ac8eb8247f377d447256cae89b358809cb631810
ks129 <45097959+ks129@users.noreply.github.com>
2020-04-24 13:26:00 +0300

    Created main game file + class

---
+++ i-dont-need-team/game/game.py
@@ -0,0 +1,12 @@
+import arcade
+
+
+class AdventuresGame(arcade.Window):
+    """Adventures of 3 Balls main game class."""
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+    def setup(self) -> None:
+        """Setup/reset game state."""
+        pass
