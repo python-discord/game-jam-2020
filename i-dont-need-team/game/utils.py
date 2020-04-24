09146851b65fe8a2ed1bf6541d8e74d7d23873ef
ks129 <45097959+ks129@users.noreply.github.com>
2020-04-24 20:08:42 +0300

    Created function `create_textures`, fixed linting

--- i-dont-need-team/game/utils.py
+++ i-dont-need-team/game/utils.py
@@ -3,16 +3,40 @@ import typing as t
 import arcade
 import PIL
 
-from .constants import TEXTURES_AND_COLORS, BLOCK_WIDTH, BLOCK_HEIGHT
+from .constants import (
+    Blocks,
+    BLOCKS_AMOUNT_X,
+    BLOCKS_AMOUNT_Y,
+    BLOCK_WIDTH,
+    BLOCK_HEIGHT,
+    TEXTURES_AND_COLORS
+)
 
 
 def create_textures() -> t.List[arcade.Texture]:
     """Create textures based on constants dictionary."""
     textures = []
-    for t in TEXTURES_AND_COLORS.values():
-        if isinstance(t, str):
+    for te in TEXTURES_AND_COLORS.values():
+        if isinstance(te, str):
             textures.append(arcade.load_texture(t))
-        elif isinstance(t, tuple):
-            img = PIL.Image.new("RGB", (BLOCK_WIDTH, BLOCK_HEIGHT), t)
-            textures.append(arcade.Texture(str(t), image=img))
+        elif isinstance(te, tuple):
+            img = PIL.Image.new("RGB", (BLOCK_WIDTH, BLOCK_HEIGHT), te)
+            textures.append(arcade.Texture(str(te), image=img))
     return textures
+
+
+def create_board() -> t.List[t.List[int]]:
+    """Initialize game board (road)."""
+    board = []
+    # Add first frame row
+    board.append([Blocks.frame for _ in range(BLOCKS_AMOUNT_X)])
+    # Create other rows
+    for _ in range(BLOCKS_AMOUNT_Y - 2):  # Remove 2 frames rows
+        board.append(
+            [Blocks.frame]
+            + [Blocks.empty for _ in range(BLOCKS_AMOUNT_X - 2)]
+            + [Blocks.frame]
+        )
+    # Add one more frame row
+    board.append([Blocks.frame for _ in range(BLOCKS_AMOUNT_X)])
+    return board
