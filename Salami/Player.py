5f44b46c40d23c2a4d7c9d010eec10850efb8a90
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-25 11:05:50 +0800

    Added dashing

--- Salami/Player.py
+++ Salami/Player.py
@@ -1,6 +1,7 @@
 
 
 import LevelGenerator
+import Textures
 
 from Constants import TILE_SIZE, ROOM_WIDTH, ROOM_HEIGHT
 from Mob import Mob
@@ -21,22 +22,40 @@ class Player(Mob):
         self.max_jump_height = 64
 
         self.walk_count = 0
-        self.walk_frame_speed = 12
+        self.walk_frame_speed = 8
         self.player_dir = True
+
+        self.curr_dash_frame = 0
+        self.max_dash_frame = 12
+        self.dashing = False
+
+        self.idle_texture = Textures.get_texture(0, 4)
+        self.idle_texture_mirrored = Textures.get_texture(0, 5)
+        self.walking_textures = Textures.get_textures(1, 4, 4)
+        self.walking_textures_mirrored = Textures.get_textures(1, 5, 4)
+        self.dash_textures = Textures.get_textures(5, 4, 3)
+        self.dash_textures_mirrored = Textures.get_textures(5, 5, 3)
+        self.crawl_textures = Textures.get_textures(8, 4, 3)
+        self.crawl_textures_mirrored = Textures.get_textures(8, 5, 3)
     
     def update(self):
+
+        speed_mult = 1
+        if self.keyboard.is_pressed("sprint"):
+            speed_mult = 2
         
         if self.keyboard.is_pressed("dash"):
-            level_gen_x = self.center_x // TILE_SIZE // ROOM_WIDTH
-            level_gen_y = self.center_y // TILE_SIZE // ROOM_HEIGHT
+            self.dashing = True
+            # level_gen_x = self.center_x // TILE_SIZE // ROOM_WIDTH
+            # level_gen_y = self.center_y // TILE_SIZE // ROOM_HEIGHT
 
-            LevelGenerator.generateLevel(self.level, int(level_gen_x), int(level_gen_y))
+            # LevelGenerator.generateLevel(self.level, int(level_gen_x), int(level_gen_y))
 
         if self.keyboard.is_pressed("attack"):
             self.level.ball.center_x = self.center_x
             self.level.ball.center_y = self.center_y
-            self.level.ball.change_x = self.change_x * 8
-            self.level.ball.change_y = self.change_y * 8
+            self.level.ball.change_x = self.change_x * 1.5
+            self.level.ball.change_y = self.change_y * 2
 
         if self.keyboard.is_pressed("jump"):
             if self.level.physics_engine.can_jump(1):
@@ -58,10 +77,6 @@ class Player(Mob):
             self.change_y = self.jump_height
             self.curr_jump_height += self.jump_height
 
-        speed_mult = 1
-        if self.keyboard.is_pressed("sprint"):
-            speed_mult = 3
-
         if self.keyboard.is_pressed("left"):
             self.change_x = -self.movespeed * speed_mult
         elif self.keyboard.is_pressed("right"):
@@ -73,5 +88,39 @@ class Player(Mob):
                 self.change_x += 1
             else:
                 self.change_x = 0
+
+        if self.dashing:
+            if self.change_x > 0:
+                self.change_x = self.movespeed * speed_mult * 2
+            elif self.change_x < 0:
+                self.change_x = -self.movespeed * speed_mult * 2
+
+            self.curr_dash_frame += 1
+            if self.curr_dash_frame >= self.max_dash_frame * len(self.dash_textures):
+                self.curr_dash_frame = 0
+                self.dashing = False
+
+        self.walk_count += 1
+        if self.walk_count >= len(self.walking_textures) * self.walk_frame_speed:     
+            self.walk_count = 0
+        
+        if self.change_x > 0:
+            if self.dashing:
+                self.texture = self.dash_textures[self.curr_dash_frame // self.max_dash_frame]
+            else:
+                self.texture = self.walking_textures[self.walk_count // self.walk_frame_speed]
+            self.player_dir = True
+
+        elif self.change_x < 0:
+            if self.dashing:
+                self.texture = self.dash_textures_mirrored[self.curr_dash_frame // self.max_dash_frame]
+            else:
+                self.texture = self.walking_textures_mirrored[self.walk_count // self.walk_frame_speed]
+            self.player_dir = False
+        else:
+            if self.player_dir:
+                self.texture = self.idle_texture
+            else:
+                self.texture = self.idle_texture_mirrored
 
         super().update()
