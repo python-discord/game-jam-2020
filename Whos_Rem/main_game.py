5d7754d045370741d50751f809d63834f6f22173
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-04-25 20:14:12 +0100

    Updated pathing

--- Whos_Rem/main_game.py
+++ Whos_Rem/main_game.py
@@ -1,19 +1,20 @@
 import arcade
-import pyautogui
 from main import *
 
 
 class Main:
-
-    size = pyautogui.size()
-
     def __init__(self):
         self.settings = Settings(self)
         self.menu = MainMenu(self)
         self.song_selection = SongSelection(self)
-        # self.play_screen = GameScreen(self)
-        self.window = arcade.Window(self.menu.width, self.menu.height,
-                                    title="3 Strings", fullscreen=True, update_rate=1/60)
+        self.window = arcade.Window(
+            #self.menu.width,
+            #self.menu.height,
+            1000,
+            600,
+            title="3 Strings",
+            fullscreen=False, update_rate=1/30)
+        self.play_screen = GameScreen(self)
 
     @property
     def brightness(self):
@@ -23,17 +24,10 @@ class Main:
     def volume(self):
         return self.settings.volume
 
-    @property
-    def width(self):
-        return self.size[0]
-
-    @property
-    def height(self):
-        return self.size[1]
-
 
 if __name__ == "__main__":
     main = Main()
-    main.window.show_view(main.menu)
+    main.window.show_view(main.play_screen)
+    main.play_screen.setup({'name': 'undertale', 'path': 'track_1', 'type': 'wav'})
     arcade.run()
 
