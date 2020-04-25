3b4eacf52d88dd232df42c7122b57373f5c4c105
Den4200 <dpham.42@hotmail.com>
2020-04-24 20:52:33 -0400

    add leaderboard button

--- monkeys-and-frogs-on-fire/triple_vision/views/leaderboard_view.py
+++ monkeys-and-frogs-on-fire/triple_vision/views/leaderboard_view.py
@@ -1,9 +1,72 @@
 import arcade
 
+from triple_vision import Settings as s
 # from triple_vision.networking import client
+
+
+class BackButton(arcade.TextButton):
+
+    def __init__(self, view, *args, **kwargs) -> None:
+        super().__init__(text='Back', *args, **kwargs)
+        self.view = view
+        self.pressed = False
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        if self.pressed:
+            self.view.back()
+            self.pressed = False
 
 
 class LeaderboardView(arcade.View):
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, main_view: arcade.View) -> None:
+        super().__init__()
+
+        self.main_view = main_view
+
+        self.back_button = None
+        self.game_title = None
+        self.background = arcade.load_texture('assets/background.png')
+
+    def back(self) -> None:
+        self.window.button_list.clear()
+        self.window.show_view(self.main_view)
+
+    def on_show(self) -> None:
+        self.back_button = BackButton(
+            self,
+            center_x=60,
+            center_y=30,
+            width=120,
+            height=60,
+            font_color=arcade.color.BLACK
+        )
+        self.window.button_list.append(self.back_button)
+
+        self.game_title = arcade.draw_text(
+            text='Triple Vision',
+            start_x=s.WINDOW_SIZE[0] / 2,
+            start_y=s.WINDOW_SIZE[1] / 8 * 7,
+            color=arcade.color.WHITE,
+            font_size=42,
+            align='center',
+            anchor_x='center',
+            anchor_y='center'
+        )
+
+    def on_draw(self) -> None:
+        arcade.start_render()
+
+        arcade.draw_lrwh_rectangle_textured(
+            bottom_left_x=0,
+            bottom_left_y=0,
+            width=s.WINDOW_SIZE[0],
+            height=s.WINDOW_SIZE[1],
+            texture=self.background
+        )
+
+        self.back_button.draw()
+        self.game_title.draw()
--- monkeys-and-frogs-on-fire/triple_vision/views/main_view.py
+++ monkeys-and-frogs-on-fire/triple_vision/views/main_view.py
@@ -2,6 +2,7 @@ import arcade
 
 from triple_vision import Settings as s
 from triple_vision.triple_vision import TripleVision
+from triple_vision.views.leaderboard_view import LeaderboardView
 
 
 class PlayButton(arcade.TextButton):
@@ -20,28 +21,73 @@ class PlayButton(arcade.TextButton):
             self.pressed = False
 
 
+class LeaderboardButton(arcade.TextButton):
+
+    def __init__(self, view, *args, **kwargs) -> None:
+        super().__init__(text='Leaderboard', *args, **kwargs)
+        self.view = view
+        self.pressed = False
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        if self.pressed:
+            self.view.leaderboard()
+            self.pressed = False
+
+
 class MainView(arcade.View):
 
     def __init__(self) -> None:
         super().__init__()
 
         self.play_button = None
+        self.leaderboard_button = None
+
+        self.game_title = None
+
         self.background = arcade.load_texture('assets/background.png')
 
     def play(self) -> None:
         self.window.button_list.clear()
         self.window.show_view(TripleVision())
 
+    def leaderboard(self) -> None:
+        self.window.button_list.clear()
+        self.window.show_view(LeaderboardView(self))
+
     def on_show(self) -> None:
         self.play_button = PlayButton(
             self,
             center_x=s.WINDOW_SIZE[0] / 2,
-            center_y=s.WINDOW_SIZE[1] / 2,
-            width=120,
+            center_y=s.WINDOW_SIZE[1] / 2 + 40,
+            width=160,
             height=60,
             font_color=arcade.color.BLACK
         )
         self.window.button_list.append(self.play_button)
+
+        self.leaderboard_button = LeaderboardButton(
+            self,
+            center_x=s.WINDOW_SIZE[0] / 2,
+            center_y=s.WINDOW_SIZE[1] / 2 - 40,
+            width=160,
+            height=60,
+            font_color=arcade.color.BLACK
+        )
+        self.window.button_list.append(self.leaderboard_button)
+
+        self.game_title = arcade.draw_text(
+            text='Triple Vision',
+            start_x=s.WINDOW_SIZE[0] / 2,
+            start_y=s.WINDOW_SIZE[1] / 8 * 7,
+            color=arcade.color.WHITE,
+            font_size=42,
+            align='center',
+            anchor_x='center',
+            anchor_y='center'
+        )
 
     def on_draw(self) -> None:
         arcade.start_render()
@@ -55,3 +101,5 @@ class MainView(arcade.View):
         )
 
         self.play_button.draw()
+        self.leaderboard_button.draw()
+        self.game_title.draw()
