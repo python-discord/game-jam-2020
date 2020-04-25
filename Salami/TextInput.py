d51cbde65d0b6d2e61a96762f191e4927b51f8cf
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-26 01:51:57 +0800

    Removed string renderinig

--- Salami/TextInput.py
+++ Salami/TextInput.py
@@ -14,7 +14,6 @@ LETTERS_CAPS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 class TextInput:
 
     def __init__(self):
-        self.text_char = []
         self.char_list = arcade.SpriteList()
 
         self.x = 32
@@ -27,12 +26,6 @@ class TextInput:
         self.char_list.move(self.x - len(self.char_list) * 4, self.y)
         self.char_list.draw(filter=gl.GL_NEAREST)
         self.char_list.move(-self.x + len(self.char_list) * 4, -self.y)
-        
-        # Draw text with arcade.draw_text
-        # arcade.draw_text(
-        #     "".join(self.text_char),
-        #     WIDTH / 2 - (len(self.text_char) / 2) * 12, HEIGHT / 2,
-        #     arcade.color.WHITE, 16)
 
     def on_key_press(self, key, modifiers):
         self.caps = modifiers == arcade.key.MOD_CAPSLOCK \
@@ -40,14 +33,10 @@ class TextInput:
             or modifiers == arcade.key.MOD_CAPSLOCK + arcade.key.MOD_SHIFT
 
         if key == arcade.key.SPACE:
-            # self.text_char.append(" ")
             # self.add_char(Textures.CHARACTERS[26])
             self.add_char(Textures.THIN_CHARS[26])
         elif key == arcade.key.BACKSPACE and len(self.char_list) > 0:
-            # self.text_char.pop()
             self.char_list.pop()
-        elif key == arcade.key.ENTER:
-            self.text_char.append("\n")
 
         elif key >= 97 and key <= 122:
             if self.caps:
@@ -55,14 +44,9 @@ class TextInput:
             else:
                 self.add_char(Textures.THIN_CHARS[key - 97])
             # self.add_char(Textures.CHARACTERS[key - 97])
-            # if self.caps:
-            #     self.text_char.append(LETTERS_CAPS[key - 97])
-            # else:
-            #     self.text_char.append(LETTERS[key - 97])
         elif key >= 44 and key <= 64:
             self.add_char(Textures.THIN_CHARS[27 * 2 + key - 44])
             # self.add_char(Textures.SYMBOLS[key - 44])
-            # self.text_char.append(SYMBOLS[key - 44])
 
     def add_char(self, texture):
         char = arcade.Sprite()
