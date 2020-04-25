bd4d3c8c68abc41b81fd90a32d99d27bbd7c734b
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-26 01:50:31 +0800

    Added entity removal and can_jump func

--- Salami/Engine.py
+++ Salami/Engine.py
@@ -1,16 +1,22 @@
 
+from Constants import TILE_SIZE
+
 class Engine:
 
-    def __init__(self, entities, tiles, gravity: float=0.0):
+    def __init__(self, entities, tiles, level, gravity: float=0.0):
         
         self.entities = entities
         self.tiles = tiles
+        self.level = level
 
         self.gravity = gravity
 
     def update(self, delta):
         
         for entity in self.entities:
+            if entity.removed:
+                self.entities.remove(entity)
+                continue
             if not entity.flying:
                 entity.change_y -= self.gravity
 
@@ -19,4 +25,20 @@ class Engine:
     def check_for_collision(self):
         pass
 
-    
+    def can_jump(self, entity, y_dist = 1):
+        x0 = int(entity.left / TILE_SIZE)
+        y0 = int(entity.bottom / TILE_SIZE)
+        x1 = int(entity.right / TILE_SIZE)
+        y1 = int(entity.top / TILE_SIZE)
+
+        tiles = self.level.get_tiles(x0, y0, x1, y1)
+
+        entity.center_y -= y_dist
+
+        for tile in tiles:
+            if entity.intersects(tile):
+                return True
+
+        entity.center_y += y_dist
+        
+        return False
