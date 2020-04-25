b212b84c79fdcb3161a5f4c7d84c25f56b14b37d
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-25 11:08:17 +0800

    Added non-solid tiles

--- Salami/Tile.py
+++ Salami/Tile.py
@@ -3,12 +3,11 @@ from Entity import Entity
 
 class Tile(Entity):
 
-    def __init__(self, tex, x, y):
-
-        super().__init__(tex, x, y)
+    def __init__(self, texture, x, y, is_solid: bool=True):
+        super().__init__(texture, x, y)
 
         self.flying = True
-    
+        self.is_solid = is_solid
     
 
     def interact(self, entity):
