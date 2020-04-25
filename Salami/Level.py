133f994d9227a52e6cc3d635dd1b85e080af623f
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-25 11:07:14 +0800

    Moved animations to Player

--- Salami/Level.py
+++ Salami/Level.py
@@ -28,29 +28,20 @@ class Level:
         self.camera = camera
         self.keyboard = keyboard
 
+        self.width = WIDTH // TILE_SIZE
+        self.height = HEIGHT // TILE_SIZE
+
         self.tile_list = arcade.SpriteList(use_spatial_hash=True, is_static=True)
         self.entities = arcade.SpriteList()
         self.tiles = {}
 
-        self.width = WIDTH // TILE_SIZE
-        self.height = HEIGHT // TILE_SIZE
+        self.rooms = []
 
         # for i in range(100):
-        #     ball = Ball(Texs.BALL, 128 + 128 * random.random(), 128 + 128 * random.random())
+        #     ball = Ball(Textures.get_texture(2, 5), 128 * random.random(), 128 * random.random())
         #     ball.change_x = random.randint(-8, 8)
         #     ball.change_y = random.randint(-8, 8)
         #     self.add_entity_to_list(ball, self.entities)
-
-        # print(f"{self.left} {self.right} {self.bottom} {self.top}")
-
-        self.idle_texture = arcade.load_texture("Salami/spritesheet.png", TILE_SIZE * 4, TILE_SIZE * 2, TILE_SIZE, TILE_SIZE)
-        self.idle_texture_mirrored = arcade.load_texture("Salami/spritesheet.png", TILE_SIZE * 4, TILE_SIZE * 2, TILE_SIZE, TILE_SIZE, mirrored=True)
-        self.walking_textures = Texs.load_textures("Salami/spritesheet.png", 4, 2, TILE_SIZE, 3)
-        self.walking_textures_mirrored = Texs.load_textures("Salami/spritesheet.png", 4, 2, TILE_SIZE, 3, mirrored=True)
-
-        self.walk_count = 0
-        self.walk_frame_speed = 12
-        self.player_dir = True
 
         self.player = Player(Textures.SPRITESHEET[4 + 3 * 16], 64, 64, self.keyboard)
         self.add_entity_to_list(self.player, self.entities)
@@ -65,14 +56,8 @@ class Level:
         #     LevelGenerator.generateRoom(self,
         #         room[0] * ROOM_WIDTH + room[0],
         #         room[1] * ROOM_HEIGHT + room[1], room[2])
-
-        for i in range(10):
-            tile = Tile(Textures.SPRITESHEET[2], random.randint(0, 15) * TILE_SIZE, 16)
-            tile.is_solid = False
-            self.add_tile(tile)
-            print(tile.width, tile.height)
         
-        self.ball = Ball(Textures.SPRITESHEET[0], self.player.center_x, self.player.center_y)
+        self.ball = Ball(Textures.SPRITESHEET[2 + 5 * 16], self.player.center_x, self.player.center_y)
         self.ball.change_x = 2
         self.add_entity_to_list(self.ball, self.entities)
 
@@ -91,23 +76,6 @@ class Level:
         # e.level = None
         # self.entities.remove(e)
 
-        self.walk_count += 1
-        if self.walk_count >= len(self.walking_textures) * self.walk_frame_speed:     
-            self.walk_count = 0
-        if self.player.change_x > 0:
-            self.player.texture = self.walking_textures[self.walk_count // self.walk_frame_speed]
-            self.player_dir = True
-
-        elif self.player.change_x < 0:
-            frame = self.walk_count // self.walk_frame_speed
-            self.player.texture = self.walking_textures_mirrored[frame]
-            self.player_dir = False
-        else:
-            if self.player_dir:
-                self.player.texture = self.idle_texture
-            else:
-                self.player.texture = self.idle_texture_mirrored
-
         self.engine.update(delta)
 
         self.count += 1
@@ -117,6 +85,7 @@ class Level:
         self.entities.draw(filter=gl.GL_NEAREST)
         self.tile_list.draw(filter=gl.GL_NEAREST)
 
+        self.camera.reset_viewport()
         # self.player.draw_hit_box(arcade.color.BLUE)
 
     def add_entity_to_list(self, entity, list):
@@ -127,7 +96,7 @@ class Level:
     def add_tile(self, tile):
         tile.set_hit_box(SQUARE_HIT_BOX)
         tile.set_level(self)
-        self._set_tile(int(tile.center_x / TILE_SIZE), int(tile.center_y / TILE_SIZE), tile)
+        self._set_tile(int(tile.center_x // TILE_SIZE), int(tile.center_y // TILE_SIZE), tile)
 
     def _set_tile(self, x: int, y: int, tile):
         # if x < 0 or y < 0 or x >= self.width or y >= self.height:
