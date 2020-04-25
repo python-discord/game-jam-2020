f8ec6fd8e9f5b9d19421e6c2f671339732928e7d
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-26 01:51:24 +0800

    Projectile class

---
+++ Salami/Projectile.py
@@ -0,0 +1,27 @@
+
+from Mob import Mob
+from Entity import Entity
+
+class Projectile(Mob):
+
+    def __init__(self, tex, x, y, dx, dy):
+        super().__init__(tex, x, y)
+
+        self.change_x = dx
+        self.change_y = dy
+
+        self.life = 600
+    
+    def update(self):
+        
+        self.life -= 1
+        if self.life < 0:
+            self.removed = True
+
+        super().update()
+    
+    def collided(self, entity, dx, dy):
+        self.change_x = 0
+        self.change_y = 0
+        self.flying = True
+        # self.removed = True
