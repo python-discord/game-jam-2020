8beefe47c02b31d4a3ebf61aaa02aabfdb87d137
Salami <48671765+markjoshua12@users.noreply.github.com>
2020-04-26 23:20:48 +0800

    Updated damage

--- Salami/SpikyBall.py
+++ Salami/SpikyBall.py
@@ -8,6 +8,14 @@ class SpikyBall(Enemy):
         super().__init__(Textures.get_texture(1, 1), x, y)
 
         self.movespeed = 2.5
+        self.damage = 2
+    
+    def update(self):
+
+        if self.intersects(self.level.player):
+            self.removed = True
+
+        super().update()
 
     def move_to(self, entity):
         if self.center_x > entity.center_x:
