6d324a8f17fbf7b6936db8f5cb2fe1ec0bb098ca
kkawabat <kakwabat@asu.edu>
2020-04-25 23:31:25 -0700

    gui_elements improvements - buttons make sound on release - refactored theme attribute to buttons

--- TriTess/game/gui_elements.py
+++ TriTess/game/gui_elements.py
@@ -1,11 +1,21 @@
+import arcade
 from arcade.gui import TextButton
-
+from arcade import Theme
+import os
+from os.path import join, dirname, realpath
 from TriTess.game.trigrid import TriGrid
+
+data_dir = join(dirname(realpath(__file__)).rsplit(os.sep, 1)[0], 'data')
+button_click = arcade.Sound(os.path.join(data_dir, "button_click.mp3"))
 
 
 class PlayHex2Btn(TextButton):
-    def __init__(self, game, x=0, y=0, width=100, height=40, text="2 Player", theme=None):
-        super().__init__(x, y, width, height, text, theme=theme)
+    def __init__(self, game, x=0, y=0, width=100, height=40):
+        theme = Theme()
+        clicked = join(data_dir, '2player_normal.png')
+        normal = join(data_dir, '2player_pressed.png')
+        theme.add_button_textures(normal, normal, clicked, clicked)
+        super().__init__(x, y, width, height, "", theme=theme)
         self.game = game
 
     def on_press(self):
@@ -13,6 +23,7 @@ class PlayHex2Btn(TextButton):
 
     def on_release(self):
         if self.pressed:
+            button_click.play()
             self.game.game_type = "hex2"
             self.game.trigrid = TriGrid(self.game.width,  self.game.height, self.game.game_type)
             self.pressed = False
@@ -20,8 +31,12 @@ class PlayHex2Btn(TextButton):
 
 
 class PlayTri3Btn(TextButton):
-    def __init__(self, game, x=0, y=0, width=100, height=40, text="3 Player", theme=None):
-        super().__init__(x, y, width, height, text, theme=theme)
+    def __init__(self, game, x=0, y=0, width=100, height=40):
+        theme = Theme()
+        clicked = join(data_dir, '3player_normal.png')
+        normal = join(data_dir, '3player_pressed.png')
+        theme.add_button_textures(normal, normal, clicked, clicked)
+        super().__init__(x, y, width, height, '', theme=theme)
         self.game = game
 
     def on_press(self):
@@ -29,6 +44,7 @@ class PlayTri3Btn(TextButton):
 
     def on_release(self):
         if self.pressed:
+            button_click.play()
             self.game.game_type = "tri3"
             self.game.trigrid = TriGrid(self.game.width,  self.game.height, self.game.game_type)
             self.pressed = False
@@ -36,15 +52,21 @@ class PlayTri3Btn(TextButton):
 
 
 class SkipTurnBtn(TextButton):
-    def __init__(self, game, x=0, y=0, width=100, height=40, text="Skip Turn", theme=None):
-        super().__init__(x, y, width, height, text, theme=theme)
+    def __init__(self, game, x=0, y=0, width=100, height=40):
+        theme = Theme()
+        clicked = join(data_dir, 'skip_normal.png')
+        normal = join(data_dir, 'skip_pressed.png')
+        theme.add_button_textures(normal, normal, clicked, clicked)
+        super().__init__(x, y, width, height, '', theme=theme)
         self.game = game
+        self.active = False
 
     def on_press(self):
         self.pressed = True
 
     def on_release(self):
         if self.pressed:
-            player_name = self.game.trigrid.next_player()
-            self.game.display_text(f"{player_name}'s turn")
+            button_click.play()
+            self.game.trigrid.clear_selection()
+            self.game.trigrid.next_player()
             self.pressed = False
