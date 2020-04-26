309854853afab4e60f342a8771553bb6b1519d3c
kkawabat <kakwabat@asu.edu>
2020-04-25 23:29:38 -0700

    updated sprite design to fit aesthetic

--- TriTess/data/sprite_unzipper.py
+++ TriTess/data/sprite_unzipper.py
@@ -9,7 +9,7 @@ def extract_sprites_zip(zip_path='sprites.zip'):
     piece_list = ['pawn', 'rook', 'king', 'bishop', 'queen', 'knight']
     sprite_file_list = []
     for piece in piece_list:
-        sprite_file_list.extend([f'sprite_{piece}{i}.png' for i in range(3)])
+        sprite_file_list.extend([f'sprite_{piece}{i}.png' for i in [0,2,1]])
 
     with zipfile.ZipFile(zip_path) as sprite_zip:
         for i, sprite_file in enumerate(sprite_zip.namelist()):
